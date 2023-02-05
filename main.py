f932e5672d4ed35b210691ffd6904b432586a04c
Toerbinator <torben.fuge@stud.hs-hannover.de>
2023-02-05 02:34:26 +0100

    Merge remote-tracking branch 'origin/main' into Torben
    # Conflicts:
    #	CSV/csvTest.csv
    #	Texts/demo.txt

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ csv1 = CSVObject()
 
 
 for x in files:
-    text = auslese(x)
-    csv1.convert(text, gui1)
+    #text = auslese(x)
+    #csv1.convert(text, gui1)
     #csv1.converter_neu(text, gui1)
     #compare_files("Texts/tabelle8.txt", "Texts/vergleich_test_tabelle8.txt")
     fach = csv1.get_kurs()
@@ -43,7 +43,21 @@ with open("output.hocr", "r") as file:
                 word = line[word_start:word_end]
                 print(f"x_wconf: {x_wconf}, Word: {word}")
 
-vergleich_csv_text()
+with open("output.hocr", "r") as file:
+    lines = file.readlines()
+    with open("hocr-confidence.txt", "w") as confidence_file:
+        for line in lines:
+            if "<span class='ocrx_word'" in line:
+                start_index = line.find("x_wconf") + 8
+                end_index = line.find("'", start_index)
+                x_wconf = int(line[start_index:end_index])
+                if x_wconf < 80:
+                    word_start = line.find(">") + 1
+                    word_end = line.find("<", word_start)
+                    word = line[word_start:word_end]
+                    confidence_file.write(f"x_wconf: {x_wconf}, Word: {word}\n")
+
+#vergleich_csv_text()
 
 """
 bild = cv2.imread("Images/test_text.png")
